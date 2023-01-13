205aef39a347c868690db89fb16ac571a95705d6
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2023-01-13 17:12:09 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'adamant'
+game 'gta5' 
+
+version '2.0.0'
+author 'TG Development'
+description 'Loading Screen for FiveM by TG Development'
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
+
+
+files {
+    'index.html',
+    'style.css',
+    'script.js',
+    'assets/logo.png',
+    'assets/image1.jpg',
+    'assets/image2.jpg',
+    'assets/image3.jpg',
+    'assets/image4.jpg',
+    'assets/image5.jpg',
+    'assets/image6.jpg',
+    'assets/image7.jpg',
+    'assets/image8.jpg',
+    'assets/image9.jpg',
+    'assets/image10.jpg',
+    'assets/discord-logo.png',
+	'assets/music.mp3'
+}
+
+client_script 'client.lua'
---
+++ script.js
@@ -0,0 +1,37 @@
+var audio = document.getElementById("audio");
+var playPauseButton = document.getElementById("play-pause-icon");
+var volumeSlider = document.getElementById("volume-slider");
+
+playPauseButton.addEventListener("click", function() {
+  if (audio.paused) {
+    audio.play();
+    playPauseButton.classList.remove("fa-play");
+    playPauseButton.classList.add("fa-pause");
+  } else {
+    audio.pause();
+    playPauseButton.classList.remove("fa-pause");
+    playPauseButton.classList.add("fa-play");
+  }
+});
+
+volumeSlider.addEventListener("input", function() {
+  audio.volume = volumeSlider.value;
+});
+
+document.body.style.overflow = "hidden";
+document.addEventListener("wheel", preventDefault, { passive: false });
+document.addEventListener("touchmove", preventDefault, { passive: false });
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
+
+
+var iconBox = document.querySelector('.blue-box');
+iconBox.addEventListener('click', redirect);
+  
+function redirect() {
+window.location.href = "http://127.0.0.1:5500/";
+}
